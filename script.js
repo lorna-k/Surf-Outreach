17b45eb0ed7a81d7a9f4ce6a21d2c5e0e02bab60
Lorna <lornanqodi@yahoo.com>
2016-07-27 21:36:39 +0200

    Website looking better

--- script.js
+++ script.js
@@ -3,9 +3,15 @@ var app = angular.module('surfApp', ['ngRoute']);
 app.config(function($routeProvider) {
 	$routeProvider.when('/', {
 		controller: 'HomeCtrl',
-		templateUrl: '/templates/Home.html'
+		templateUrl: '/templates/home.html'
+	}).when('/surfkids', {
+		controller: 'SurfKidsCtrl',
+		templateUrl: '/templates/surfkids.html'
 	})
 });
 
 app.controller('HomeCtrl', function($scope) {
+});
+
+app.controller('SurfKidsCtrl', function($scope) {
 });
